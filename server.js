6734199112a11b7655b74c2d0d2e74aba3638167
Levi-F06 <levi30flanagan@icloud.com>
2025-04-02 12:26:29 +0100

    ✨ Create file server

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-const http = require("http");
+import { createServer } from "http";
+import { readFile } from "fs";
 
-http.createServer((req, res) => {
-  res.write("hello world");
-  res.end();
+const INDEX_PAGE = "/src/html/index.html";
+
+createServer((req, res) => {
+  const url = req.url === "/" ? INDEX_PAGE : req.url;
+  console.log(req.url);
+  console.log(url);
+  readFile(`.${url}`, (_, data) => {
+    res.write(data);
+    res.end();
+  })
 }).listen(8000);
