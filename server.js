cc3b382330c7bf01e3bd201b3d5f9c24203211b8
Levi-F06 <levi30flanagan@icloud.com>
2025-04-23 11:04:18 +0100

    ✨ Create room select page

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 import { createServer } from "http";
 import { readFile } from "fs";
+import url from 'node:url';
 
 const INDEX_PAGE = "/src/html/index.html";
 
 createServer((req, res) => {
-  const url = req.url === "/" ? INDEX_PAGE : req.url;
-  console.log(req.url);
-  console.log(url);
-  readFile(`.${url}`, (e, data) => {
-    if (e) {
-      console.log(e);
+   
+  const path = url.parse(req.url === "/" ? INDEX_PAGE : req.url);
+  readFile(`.${path.pathname}`, (err, data) => {
+    if (err) {
+      // even if error is intended this will just avoid the server crashing
+      console.log(err);
     } else {
       res.write(data);
       res.end();
