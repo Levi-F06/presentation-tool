e260799be7b257f117e5ebb6868a402c898a3b90
Levi-F06 <levi30flanagan@icloud.com>
2025-04-23 09:42:01 +0100

    🐛 Fix issue with server

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ createServer((req, res) => {
   const url = req.url === "/" ? INDEX_PAGE : req.url;
   console.log(req.url);
   console.log(url);
-  readFile(`.${url}`, (_, data) => {
-    res.write(data);
-    res.end();
+  readFile(`.${url}`, (e, data) => {
+    if (e) {
+      console.log(e);
+    } else {
+      res.write(data);
+      res.end();
+    }
   })
 }).listen(8000);
