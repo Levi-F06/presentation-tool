dfc3e6166d8cbcfcb88d0bc481c7e32563a54214
Levi-F06 <levi30flanagan@icloud.com>
2025-06-05 22:26:29 +0100

    feat: Retrive relavant data from server
    This also moved the code from get-data.js into main

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,12 @@ const [
   time,
 ] = document.querySelectorAll(".dynamic");
 
+// made the params numbers so now we have this!!
+const dayMap = {
+  "1": "one",
+  "2": "two",
+};
+
 function updateClock() {
   const currentTime = new Date();
   time.innerHTML = `
@@ -22,5 +28,28 @@ function updateClock() {
   `;
 }
 
-updateClock();
-setInterval(updateClock, 1000);
+async function getData() {
+  const params = new URL(globalThis.location.href).searchParams;
+  const room = params.get("room");
+  const day = dayMap[params.get("day")];
+
+  const res = await fetch(`/schedules/${room}.JSON`);
+  if (!res.ok) {
+    alert(
+      "There was an error retriving this data, try going back to the start screen :(",
+    );
+  } else {
+    const json = await res.json().then((res) => res);
+    // eg day 1 in the json
+    return json[`day ${day}`];
+  }
+}
+
+async function main() {
+  const data = await getData();
+  console.log(data);
+  updateClock();
+  setInterval(updateClock, 1000);
+}
+
+main();
